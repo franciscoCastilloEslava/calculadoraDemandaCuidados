7fa80ede90cba6df9aae1a57383ccdf5da9c51da
Jesus Manuel Castillo Eslava <jcastile@everis.com>
2020-04-11 13:33:09 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,6 @@ import { LoginComponent } from './features/login/login.component';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  }, {
     path: 'home',
     component: LayoutComponent,
     children: [
@@ -26,6 +22,10 @@ const routes: Routes = [
   }, {
     path: 'login',
     component: LoginComponent
+  }, {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
   }, {
     path: '**',
     component: LoginComponent
